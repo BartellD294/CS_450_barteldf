e2024f5c1249587906dade36fc2f92312a6d39c3
Dylan Bartell <barteldf@sunypoly.edu>
2024-04-09 21:30:34 -0400

    Assiugnment should be finished

--- shaders/Assign06/Basic.fs
+++ shaders/Assign06/Basic.fs
@@ -19,14 +19,15 @@ void main()
 {
 	vec4 N = normalize(interNormal);
 	vec3 L = normalize(vec3(light.pos-interPos));
-	vec3 H = normalize(vec3(interPos) + L);
+	vec3 H = normalize(vec3(-interPos) + L);	//distance from intersection to eye = -interPos
 	float diffuseCoefficient = max(0, dot(vec3(N),L));
-	vec3 diffColor = vec3(diffuseCoefficient * vertexColor * light.color);
+	vec3 diffColor = vec3(vertexColor * light.color * diffuseCoefficient);
 	float shininess = 10.0;
-	float specularCoefficient = diffuseCoefficient * pow(max(0, dot(vec3(N),H)), shininess);
+	float preSpec = max(0, dot(vec3(N), H));
+	float specularCoefficient = diffuseCoefficient * pow(preSpec, shininess);
 	vec3 specularColor = vec3(vec3(1.0,1.0,1.0) * vec3(light.color) * specularCoefficient);
 
 	// Just output interpolated color
-	out_color = vec4(vec3(H ), 1.0);
+	out_color = vec4(vec3(diffColor + specularColor ), 1.0);
 
 }
